b5e3383accf9242bfbbc121e5631963e1b1b2eee
Fabian Sato <40687901+fabiansato@users.noreply.github.com>
2022-01-23 09:30:51 -0300

    Ejercicios C
    Ejercicios C

---
+++ A01-obtener-datos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  char nombre[25];
+  printf("Escribe tu nombre:\n");
+  scanf("%s",nombre);  
+  printf("Hola  %s",nombre);
+  printf(" bienvenido al mundo de la programaci%cn",162);
+  return 0;
+  
+}
